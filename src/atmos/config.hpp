a61d6d60f69e08c26d830f8c5643a6b53c21571e
Andrey Suvalov <suvalov.andrew@gmail.com>
2022-03-26 08:13:56 +0100

    Minor changes

---
+++ src/atmos/config.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string_view>
+
+constexpr std::string_view host = "api.openweathermap.org";
+constexpr std::string_view port = "80";
+constexpr std::string_view target = "/data/2.5/weather";
+
+constexpr std::string_view token = "e5b292ae2f9dae5f29e11499c2d82ece";
+constexpr std::string_view city = "Minsk";
+const int version = 10;
+
+constexpr std::wstring_view cels_deg_sym = L"℃";
+constexpr std::wstring_view wind_icon = L"💨";
